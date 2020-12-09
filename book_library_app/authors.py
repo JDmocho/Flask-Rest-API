fa28a7065215c8ce05ec4636704af143acea3346
JDmocho <joanna.dmocho@gmail.com>
2020-12-09 21:17:40 +0100

    chore: add post put delete methods

--- book_library_app/authors.py
+++ book_library_app/authors.py
@@ -1,41 +1,71 @@
-from flask import jsonify
-from book_library_app import app
+from flask import jsonify, request
+from webargs.flaskparser import use_args
+from book_library_app import app, db
+from book_library_app.models import Author, AuthorSchema, author_schema
+from book_library_app.utils import validate_json_content_type
 
 
 @app.route('/api/v1/authors', methods=['GET'])
 def get_authors():
+    authors = Author.query.all()
+    author_schema = AuthorSchema(many=True)
+
     return jsonify({
         'success': True,
-        'data': 'Get all authors'
+        'data': author_schema.dump(authors),
+        'number_of_records': len(authors)
+
     })
 
 
 @app.route('/api/v1/authors/<int:author_id>', methods=['GET'])
 def get_author(author_id: int):
+    author = Author.query.get_or_404(author_id, description=f'Author with id {author_id} not found')
     return jsonify({
         'success': True,
-        'data': f'Get single author with id {author_id}'
+        'data': author_schema.dump(author)
     })
 
 
 @app.route('/api/v1/authors', methods=['POST'])
-def create_author():
+@validate_json_content_type
+@use_args(author_schema, error_status_code=400)
+def create_author(args: dict):
+    author = Author(**args)
+
+    db.session.add(author)
+    db.session.commit()
+
     return jsonify({
         'success': True,
-        'data': 'New author has been created'
+        'data': author_schema.dump(author)
     }), 201
 
 
 @app.route('/api/v1/authors/<int:author_id>', methods=['PUT'])
-def update_author(author_id: int):
+@validate_json_content_type
+@use_args(author_schema, error_status_code=400)
+def update_author(args: dict, author_id: int):
+    author = Author.query.get_or_404(author_id, description=f'Author with id {author_id} not found')
+
+    author.first_name = args['first_name']
+    author.last_name = args['last_name']
+    author.birth_date = args['birth_date']
+
+    db.session.commit()
     return jsonify({
         'success': True,
-        'data': f'Author with id {author_id} has been updated'
+        'data': author_schema.dump(author)
     })
 
 
 @app.route('/api/v1/authors/<int:author_id>', methods=['DELETE'])
 def delete_author(author_id: int):
+    author = Author.query.get_or_404(author_id, description=f'Author with id {author_id} not found')
+
+    db.session.delete(author)
+    db.session.commit()
+
     return jsonify({
         'success': True,
         'data': f'Author with id {author_id} has been deleted'
