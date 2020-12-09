0643b8f9d8a9ef6d0929c6b882d628d0ff206668
JDmocho <joanna.dmocho@gmail.com>
2020-12-09 21:31:12 +0100

    chore: class AuthorSchema

--- book_library_app/models.py
+++ book_library_app/models.py
@@ -1,4 +1,7 @@
 from book_library_app import db
+from datetime import datetime
+from marshmallow import Schema, fields, validate, validates, ValidationError
+
 
 class Author(db.Model):
     __tablename__ = 'authors'
@@ -9,3 +12,18 @@ class Author(db.Model):
 
     def __repr__(self):
         return f'<{self.__class__.__name__}>: {self.first_name} {self.last_name}'
+
+
+class AuthorSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    first_name = fields.String(required=True,validate=validate.Length(max=50))
+    last_name = fields.String(required=True, validate=validate.Length(max=50))
+    birth_date = fields.Date('%d-%m-%Y', required=True)
+
+    @validates('birth_date')
+    def validate_birth_date(self, value):
+        if value > datetime.now().date():
+            raise ValidationError(f'Birth date must be lower than {datetime.now().date()}')
+
+
+author_schema = AuthorSchema()
