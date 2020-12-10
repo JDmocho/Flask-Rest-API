acdfef7307f801c82b09f4c253e2bb19badc624a
JDmocho <joanna.dmocho@gmail.com>
2020-12-10 22:48:02 +0100

    chore: filters implementation

--- book_library_app/models.py
+++ book_library_app/models.py
@@ -1,6 +1,13 @@
+import re
+from flask_sqlalchemy import BaseQuery
+from sqlalchemy.orm.attributes import InstrumentedAttribute
+from sqlalchemy.sql.expression import BinaryExpression
 from book_library_app import db
 from datetime import datetime
 from marshmallow import Schema, fields, validate, validates, ValidationError
+from werkzeug.datastructures import ImmutableDict
+
+COMMPARISON_OPERATORS_RE = re.compile(r'(.*)\[(gte|gt|lte|lt)\]')
 
 
 class Author(db.Model):
@@ -13,10 +20,69 @@ class Author(db.Model):
     def __repr__(self):
         return f'<{self.__class__.__name__}>: {self.first_name} {self.last_name}'
 
+    @staticmethod
+    def get_schema_args(fields: str) -> dict:
+        schema_args = {'many': True}
+        if fields:
+            fileds_only = []
+            for field in fields.split(','):
+                if field in Author.__table__.columns:
+                    fileds_only.append(field)
+
+            schema_args['only'] = fileds_only
+        # print(schema_args)
+        return schema_args
+
+    @staticmethod
+    def apply_order(query: BaseQuery, sort_keys: str) -> BaseQuery:
+        if sort_keys:
+            for key in sort_keys.split(','):
+                desc = False
+                if key.startswith('-'):
+                    key = key[1:]
+                    desc = True
+                column_attr = getattr(Author, key, None)
+                if column_attr is not None:
+                    if desc:
+                        query = query.order_by(column_attr.desc())
+                    else:
+                        query.order_by(column_attr)
+        return query
+
+    @staticmethod
+    def get_filter_argument(column_name: InstrumentedAttribute, value: str, operator: str) -> BinaryExpression:
+        operator_mapping = {
+            '==': column_name == value,
+            'gte': column_name >= value,
+            'gt': column_name > value,
+            'lte': column_name <= value,
+            'lt': column_name < value
+        }
+        return operator_mapping[operator]
+
+    @staticmethod
+    def apply_filter(query: BaseQuery, params: ImmutableDict) -> BaseQuery:
+        for param, value in params.items():
+            if param not in {'fields', 'sort'}:
+                operator = '=='
+                match = COMMPARISON_OPERATORS_RE.match(param)
+                if match is not None:
+                    param, operator = match.groups()
+                column_attr = getattr(Author, param, None)
+                if column_attr is not None:
+                    if param == 'birth_date':
+                        try:
+                            value = datetime.strptime(value, '%d-%m-%Y').date()
+                        except ValidationError:
+                            continue
+                    filter_argument = Author.get_filter_argument(column_attr, value, operator)
+                    query = query.filter(filter_argument)
+        return query
+
 
 class AuthorSchema(Schema):
     id = fields.Integer(dump_only=True)
-    first_name = fields.String(required=True,validate=validate.Length(max=50))
+    first_name = fields.String(required=True, validate=validate.Length(max=50))
     last_name = fields.String(required=True, validate=validate.Length(max=50))
     birth_date = fields.Date('%d-%m-%Y', required=True)
 
