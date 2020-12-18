7f79b3771e696d38d52a822d42efabe084de7f40
JDmocho <joanna.dmocho@gmail.com>
2020-12-18 16:04:56 +0100

    chore: user registration

--- book_library_app/models.py
+++ book_library_app/models.py
@@ -1,6 +1,11 @@
-from book_library_app import db
-from datetime import datetime, date
+import jwt
+from flask import current_app
+
+from datetime import datetime, date, timedelta
 from marshmallow import Schema, fields, validate, validates, ValidationError
+from werkzeug.security import generate_password_hash
+
+from book_library_app import db
 
 
 class Author(db.Model):
@@ -49,6 +54,18 @@ class User(db.Model):
     email = db.Column(db.String(255), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
     create_date = db.Column(db.DateTime(255), default=datetime.utcnow)
+
+    @staticmethod
+    def generate_hashed_password(password: str) -> str:
+        return generate_password_hash(password)
+
+    def generate_jwt(self) -> bytes:
+        payload = {
+            'user_id': self.id,
+            'exp': datetime.utcnow() + timedelta(minutes=current_app.config.get('JWT_EXPIRED_MINUTES', 30))
+
+        }
+        return jwt.encode(payload, current_app.config.get('SECRET_KEY'))
 
 
 class AuthorSchema(Schema):
