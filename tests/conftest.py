f7bb938f2696f62b513c45abe36d9390971b96b2
JDmocho <joanna.dmocho@gmail.com>
2020-12-21 19:09:44 +0100

    chore: tests for the register function

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,8 @@ import pytest
 
 from book_library_app import create_app, db
 
-@pytest.fixture()
+
+@pytest.fixture
 def app():
     app = create_app('testing')
 
@@ -12,3 +13,20 @@ def app():
     yield app
 
     app.config['DB_FILE_PATH'].unlink(missing_ok=True)
+
+
+@pytest.fixture
+def client(app):
+    with app.test_client() as client:
+        yield client
+
+
+@pytest.fixture()
+def user(client):
+    user = {
+        'username': 'test25',
+        'password': ' 123456',
+        'email': 'test25@gmail.com'
+    }
+    client.post('/api/v1/auth/register', json=user)
+    return user
