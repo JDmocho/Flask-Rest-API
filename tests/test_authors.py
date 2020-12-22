f52c53fb05b1a4c51bf47d61618f0d9995f3b382
JDmocho <joanna.dmocho@gmail.com>
2020-12-22 10:03:48 +0100

    chore: tests for the get_author function

--- tests/test_authors.py
+++ tests/test_authors.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_get_authors_no_records(client):
     response = client.get('/api/v1/authors')
     expected_result = {
@@ -54,3 +57,24 @@ def test_get_authors_with_params(client, sample_data):
             'first_name': 'Dan'
         }
     ]
+
+
+def test_get_single_author(client, sample_data):
+    response = client.get('/api/v1/authors/9')
+    response_data = response.get_json()
+    assert response.status_code == 200
+    assert response.headers['Content-Type'] == 'application/json'
+    assert response_data['success'] is True
+    assert response_data['data']['first_name'] == 'Andrzej'
+    assert response_data['data']['last_name'] == 'Sapkowski'
+    assert response_data['data']['birth_date'] == '21-06-1948'
+    assert len(response_data['data']['books']) == 1
+
+
+def test_get_single_author_not_found(client, sample_data):
+    response = client.get('/api/v1/authors/30')
+    response_data = response.get_json()
+    assert response.status_code == 404
+    assert response.headers['Content-Type'] == 'application/json'
+    assert response_data['success'] is False
+    assert 'data' not in response_data
